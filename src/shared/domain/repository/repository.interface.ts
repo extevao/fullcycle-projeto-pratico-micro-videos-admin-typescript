1b9f96691948754aeb2be5cf05134329a7bda8f6
Estevao <estevaovasques@hotmail.com>
2024-02-18 15:12:41 +0000

    Criando interface de repositorio

--- src/shared/domain/repository/repository.interface.ts
+++ src/shared/domain/repository/repository.interface.ts
@@ -1,5 +1,7 @@
 import { Entity } from "../entity"
 import { ValueObject } from "../value-object"
+import { SearchParams } from "./search-params"
+import { SearchResult } from "./search-result"
 
 export interface IRepository<E extends Entity, EntityId extends ValueObject> {
   insert(entity: E): Promise<void>
@@ -12,4 +14,17 @@ export interface IRepository<E extends Entity, EntityId extends ValueObject> {
   findAll(): Promise<E[]>
 
   getEntity(): new (...args: any[]) => E
+}
+
+
+export interface ISearcheableRepository<
+  E extends Entity,
+  EntityId extends ValueObject,
+  SearchInput = SearchParams,
+  SearchOutput = SearchResult
+> extends IRepository<E, EntityId> {
+
+  sortableFields: string[];
+
+  search(props: SearchInput): Promise<SearchOutput>
 }
