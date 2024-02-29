f2760002366fc856724c3a0ddec289b9b299d96c
Estevao <estevaovasques@hotmail.com>
2024-02-29 00:56:26 +0000

    Finalizado repositorio categoria

--- src/category/domain/category.repository.ts
+++ src/category/domain/category.repository.ts
@@ -1,5 +1,22 @@
-import { IRepository } from "../../shared/domain/repository/repository.interface";
+import { ISearcheableRepository } from "../../shared/domain/repository/repository.interface";
+import { SearchParams } from "../../shared/domain/repository/search-params";
+import { SearchResult } from "../../shared/domain/repository/search-result";
 import { Uuid } from "../../shared/domain/values-objects/uuid.vo";
 import { Category } from "./category.entity";
 
-export interface CategoryRepository extends IRepository<Category, Uuid> { }
+
+export type CategoryFilter = string
+
+export class CategorySearchParams extends SearchParams<CategoryFilter> { }
+
+export class CategorySearchResult extends SearchResult<Category> { }
+
+export interface ICategoryRepository
+  extends ISearcheableRepository<
+    Category,
+    Uuid,
+    CategoryFilter,
+    CategorySearchParams,
+    CategorySearchResult
+  > { }
+
--- src/category/infra/db/in-memory/category-in-memory.repository.ts
+++ src/category/infra/db/in-memory/category-in-memory.repository.ts
@@ -2,10 +2,16 @@ import { SortDirection } from "../../../../shared/domain/repository/search-param
 import { Uuid } from "../../../../shared/domain/values-objects/uuid.vo";
 import { InMemorySearchableRepository } from "../../../../shared/infra/db/in-memory/in-memory.repository";
 import { Category } from "../../../domain/category.entity";
+import { CategoryFilter, ICategoryRepository } from "../../../domain/category.repository";
 
-export class CategoryInMemoryRespository extends InMemorySearchableRepository<Category, Uuid> {
+export class CategoryInMemoryRespository
+  extends InMemorySearchableRepository<Category, Uuid>
+  implements ICategoryRepository {
 
-  protected async applyFilter(items: Category[], filter: string): Promise<Category[]> {
+  protected async applyFilter(
+    items: Category[],
+    filter: CategoryFilter
+  ): Promise<Category[]> {
     if (!filter) {
       return items
     }
